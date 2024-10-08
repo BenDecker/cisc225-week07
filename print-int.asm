fc4261a605a57a88d98bd88386c29e5650bc2517
Ben Decker <143534537+BenDecker@users.noreply.github.com>
2024-10-08 17:46:54 +0000

    end of class

--- print-int.asm
+++ print-int.asm
@@ -9,8 +9,22 @@ N equ 36247890
 
 _start:
 
+    mov rax,1653423         ; output this nubmer to console in decimal form
 
+    mov rcx,buf_len         ; set up for loop counter
+    sub rcx,1               ; loop counter (number of digits in buf)
+    mov rdi,buf             ; address for output buffer
+    add rdi,buff_len        
+    sub rdi,2               ; address for right most digit
 
+    mov rbx,10              ; divisor for extracting digits
+L1:
+    mov rdx,0               ; set up high 64-bits for division
+    div rbx                 ; divide rax by 10
+    add dl,'0'              ; convert remainder to ASCII character
+    mov [rdi],dl            ; move character to output buffer
+    dec rdi
+    loop L1                 ; get next digit
 
     ; Write buf to the console
     mov rax,1               ; system call for write 
@@ -27,5 +41,5 @@ _start:
 section .data           ; start of initialized data segment
 
     buf db "xxxxxxxxxx",0xa     ; output string with newline
-
+    buf_len equ $-buf
 section .bss            ; start of uninitialized data segment 
--- sum1n.asm
+++ sum1n.asm
@@ -8,7 +8,11 @@ N equ 10 ; Calculate sum of 1 through N, leave result in RAX
 
 _start:
 
-
+    mov rax,[num]
+    mov rbx,rax
+    inc rbx
+    mul rbx
+    shr rax,1
 
     ; End the program
     mov rax,0x3c            ; system call for exit 
@@ -16,5 +20,7 @@ _start:
     syscall                 ; invoke operating system call 
 
 section .data           ; start of initialized data segment
+
+    num dq 4
 
 section .bss            ; start of uninitialized data segment 
